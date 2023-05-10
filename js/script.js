89527dd970d81f17ceb35d10a700c4289d2c3c38
Jackiso2006 <isocomo06@gmail.com>
2023-05-10 15:59:22 +0200

    aggiunta funzione checkCollision

--- js/script.js
+++ js/script.js
@@ -177,6 +177,17 @@ function resumeTimer(timeInSeconds) {
     }, 1000);
 }
 
+function checkCollision(oggetto1, oggetto2) {
+    var obj1 = oggetto1.getBoundingClientRect();
+    var obj2 = oggetto2.getBoundingClientRect();
+    return (
+        obj1.x < obj2.x + obj2.width &&
+        obj1.x + obj1.width > obj2.x &&
+        obj1.y < obj2.y + obj2.height &&
+        obj1.y + obj1.height > obj2.y
+    );
+}
+
 function changeScore(scoreDiv, scorePoints) {
 
     scoreDiv.textContent = "Score : " + scorePoints;
